ab7735bc8107892ebe4438abbfbf0b6f33e6f965
PravallikaGit23 <155510880+PravallikaGit23@users.noreply.github.com>
2024-01-03 12:29:10 +0530

    Update Main.java
    "implemented main method

--- IdeaProjects/untitled/src/Main.java
+++ IdeaProjects/untitled/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("new file");
+        Animal a = (name)->{System.out.println(name)};
+        a.name(pravallika);
+        
     }
 }
